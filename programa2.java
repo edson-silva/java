9b2a404ac96df51dc5a4b68044650054fbba05f1
edson-silva <81913175+edson-silva@users.noreply.github.com>
2022-05-04 17:36:06 -0300

    Add files via upload

---
+++ programa2.java
@@ -0,0 +1,25 @@
+
+
+
+public class programa2 {
+
+    public static void main(String args[] ) {
+
+    int a, b, c;
+
+    a = 2;
+    b = 3;
+    c = 4;
+
+    if (a < b) System.out.println("a é menor que b");
+     
+    if (a == b ) System.out.println (" a é igual a b");
+
+
+
+    }
+
+
+
+
+}
