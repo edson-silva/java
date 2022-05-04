5dccc77e821ebae1b368586378e1f5f4a698836a
edson-silva <81913175+edson-silva@users.noreply.github.com>
2022-05-04 20:54:11 -0300

    Add files via upload

---
+++ programa6.java
@@ -0,0 +1,53 @@
+
+// Demonstra Switch.
+
+class SwitchDemo { 
+
+    public static void main (String args[]) {
+
+        int i;
+        
+        for(i = 0; i < 10; i++) 
+
+        switch (i) {
+
+        case 0:
+
+        System.out.println(" i é Zero");
+
+        break;
+
+        case 1:
+        System.out.println(" i é  Um");
+
+        break;
+
+        case 2:
+        System.out.println("i é Dois");
+
+        break;
+
+        case 3:
+        System.out.println(" i é Três");
+
+        break;
+
+        case 4:
+        System.out.println(" i é Quatro");
+
+        break;
+
+        default:
+
+        System.out.println("i é Cinco ou mais");
+
+
+        }
+
+
+    }
+ 
+
+}
+
+
